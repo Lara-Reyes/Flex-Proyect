335c6e8edfa35eefdd6571e635b6603fdb75b87b
Lara Reyes <126874982+Lara-Reyes@users.noreply.github.com>
2023-06-04 15:19:30 -0300

    proyecto

---
+++ ProyectoIntegrador.js
@@ -0,0 +1,17 @@
+    let modal = document.getElementById("ventanaModal");
+    let boton = document.getElementById("abrirModal");
+    let span = document.getElementsByClassName("close")[0];
+
+    boton.addEventListener("click", function () {
+      modal.style.display = "block";
+    });
+
+    span.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
